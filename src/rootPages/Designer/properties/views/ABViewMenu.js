db197597c7938b87083a5f0c0dfb75be9ffaa87d
Johnny Hausman <johnny.hausman@cru.org>
2026-02-14 22:09:34 -0600

    [fix] new plugin architecture should pull views from AB.ClassManager.viewClass()

--- src/rootPages/Designer/properties/views/ABViewMenu.js
+++ src/rootPages/Designer/properties/views/ABViewMenu.js
@@ -775,7 +775,9 @@ export default function (AB) {
          });
 
          // add tabs
-         const ABViewTab = this.AB.Class.ABViewManager.viewClass("tab");
+         // ABViewManager.viewClass() is depreciated.  Use ClassManager.viewClass() instead.
+         //const ABViewTab = this.AB.Class.ABViewManager.viewClass("tab");
+         const ABViewTab = this.AB.ClassManager.viewClass("tab");
          page
             .views((v) => v instanceof ABViewTab)
             .forEach((tab, tabIndex) => {
