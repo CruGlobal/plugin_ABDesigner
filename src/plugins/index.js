b103ef58414fd7de9f9c662eba65b7832bf22589
roguisharcanetrickster <roguisharcanetrickster@gmail.com>
2026-05-18 15:34:08 +0700

    kanban

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,55 +1,59 @@
-import viewPivotProperties from "./web_view_pivot/FNAbviewpivot.js";
-import viewPivotEditor from "./web_view_pivot/FNAbviewpivotEditor.js";
-import viewCarouselProperties from "./web_view_carousel/FNAbviewcarousel.js";
 import viewCarouselEditor from "./web_view_carousel/FNAbviewcarouselEditor.js";
-import viewCommentProperties from "./web_view_comment/FNAbviewcomment.js";
+import viewCarouselProperties from "./web_view_carousel/FNAbviewcarousel.js";
 import viewCommentEditor from "./web_view_comment/FNAbviewcommentEditor.js";
-import viewDataSelectProperties from "./web_view_data-select/FNAbviewdataselect.js";
+import viewCommentProperties from "./web_view_comment/FNAbviewcomment.js";
 import viewDataSelectEditor from "./web_view_data-select/FNAbviewdataselectEditor.js";
+import viewDataSelectProperties from "./web_view_data-select/FNAbviewdataselect.js";
 import viewDataviewProperties from "./web_view_dataview/FNAbviewdataview.js";
 import viewDataviewEditor from "./web_view_dataview/FNAbviewdataviewEditor.js";
 import viewDetailProperties from "./web_view_detail/FNAbviewdetail.js";
 import viewDetailEditor from "./web_view_detail/FNAbviewdetailEditor.js";
-import viewImageProperties from "./web_view_image/FNAbviewimage.js";
 import viewImageEditor from "./web_view_image/FNAbviewimageEditor.js";
-import viewLabelProperties from "./web_view_label/FNAbviewLabel.js";
+import viewImageProperties from "./web_view_image/FNAbviewimage.js";
+import viewKanbanEditor from "./web_view_kanban/FNAbviewkanbanEditor.js";
+import viewKanbanProperties from "./web_view_kanban/FNAbviewkanban.js";
 import viewLabelEditor from "./web_view_label/FNAbviewLabelEditor.js";
-import viewLayoutProperties from "./web_view_layout/FNAbviewlayout.js";
+import viewLabelProperties from "./web_view_label/FNAbviewLabel.js";
 import viewLayoutEditor from "./web_view_layout/FNAbviewlayoutEditor.js";
+import viewLayoutProperties from "./web_view_layout/FNAbviewlayout.js";
 import viewListProperties from "./web_view_list/FNAbviewlist.js";
-import viewPdfImporterProperties from "./web_view_pdfImporter/FNAbviewpdfimporter.js";
 import viewPdfImporterEditor from "./web_view_pdfImporter/FNAbviewpdfimporterEditor.js";
-import viewTabProperties from "./web_view_tab/FNAbviewtab.js";
+import viewPdfImporterProperties from "./web_view_pdfImporter/FNAbviewpdfimporter.js";
+import viewPivotProperties from "./web_view_pivot/FNAbviewpivot.js";
+import viewPivotEditor from "./web_view_pivot/FNAbviewpivotEditor.js";
 import viewTabEditor from "./web_view_tab/FNAbviewtabEditor.js";
-import viewTextProperties from "./web_view_text/FNAbviewtext.js";
+import viewTabProperties from "./web_view_tab/FNAbviewtab.js";
 import viewTextEditor from "./web_view_text/FNAbviewtextEditor.js";
+import viewTextProperties from "./web_view_text/FNAbviewtext.js";
 
 const AllPlugins = [
-   viewCarouselProperties,
    viewCarouselEditor,
-   viewCommentProperties,
+   viewCarouselProperties,
    viewCommentEditor,
-   viewDataSelectProperties,
+   viewCommentProperties,
    viewDataSelectEditor,
+   viewDataSelectProperties,
    viewDataviewProperties,
    viewDataviewEditor,
    viewDetailProperties,
    viewDetailEditor,
-   viewImageProperties,
    viewImageEditor,
-   viewLabelProperties,
+   viewImageProperties,
+   viewKanbanEditor,
+   viewKanbanProperties,
    viewLabelEditor,
-   viewLayoutProperties,
+   viewLabelProperties,
    viewLayoutEditor,
+   viewLayoutProperties,
    viewListProperties,
-   viewPdfImporterProperties,
    viewPdfImporterEditor,
-   viewTabProperties,
-   viewTabEditor,
-   viewTextProperties,
-   viewTextEditor,
+   viewPdfImporterProperties,
    viewPivotProperties,
    viewPivotEditor,
+   viewTabEditor,
+   viewTabProperties,
+   viewTextEditor,
+   viewTextProperties,
 ];
 
 export default {
